cc08217312588b6c56b1f3e29c17b6dd0bf600ed
AnandDev <15194725+anandrathidev@users.noreply.github.com>
2025-06-11 01:45:59 +0530

    Update CallThreadOneafterOther.cpp

--- CallThreadOneafterOther.cpp
+++ CallThreadOneafterOther.cpp
@@ -7,16 +7,6 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
-
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <iostream>
 #include <chrono>
 #include <thread>
 #include <mutex>
@@ -36,14 +26,13 @@ void Print_and_incr_t1()
     {
         try{
             std::unique_lock<std::mutex> lg{mi};
-            std::cout << "Acq lock   and wait condi producer 1  i :" << i << "\n";
+            std::cout << "Lock acq and wait producer 1  i :" << i << "\n";
             c1.wait(lg);
-            
             ++i;
             std::cout << "producer 1  i :" << i << "\n";
             lg.unlock();
+            std::cout << "producer 1 Notify producer 2  :"  << "\n";
             c2.notify_one();
-            std::cout << "Unlock and  notify producer 2  :" << "\n";
         }
         catch(std::exception &e)
         {
@@ -60,14 +49,14 @@ void Print_and_incr_t2()
         try{
             //std::unique_lock<std::mutex> lg{mi , std::defer_lock};
             std::unique_lock<std::mutex> lg{mi};
-            std::cout << "Acq lock   and wait condi producer 2  i :" << i << "\n";
+            std::cout << "Lock acq and wait producer 2  i :" << i << "\n";
 
             c2.wait(lg);
             ++i;
             std::cout << "producer 2  i :" << i << "\n";
             lg.unlock();
+            std::cout << "producer 2 Notify producer 3  :"  << "\n";
             c3.notify_one();
-            std::cout << "Unlock and notify producer 3  "  << "\n";
         }
         catch(std::exception &e)
         {
@@ -82,14 +71,14 @@ void Print_and_incr_t3()
     while(i<100)
     {
         try{
+            //std::unique_lock<std::mutex> lg{mi , std::defer_lock};
             std::unique_lock<std::mutex> lg{mi};
-            std::cout << "Acq lock  and wait condi  producer 3 :" << i << "\n";
+            std::cout << "Lock acq and wait producer 3  i :" << i << "\n";
             c3.wait(lg);
             ++i;
-            std
-            ::cout << "producer 3  i :" << i << "\n";
+            std::cout << "producer 3  i :" << i << "\n";
             lg.unlock();
-            std::cout << "Unlock and notify producer 1  :" << "\n";
+            std::cout << "producer 3 Notify producer 1  :"  << "\n";
             c1.notify_one();
         }
         catch(std::exception &e)
@@ -106,9 +95,8 @@ int main()
     std::thread t1{Print_and_incr_t1};
     std::thread t2{Print_and_incr_t2};
     std::thread t3{Print_and_incr_t3};
-
     using namespace std::chrono_literals;
-   std::this_thread::sleep_for(2000ms);
+    std::this_thread::sleep_for(2000ms);
     c1.notify_one();
 
     t1.join(); 
